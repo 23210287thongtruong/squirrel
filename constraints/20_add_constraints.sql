807eb09f360bb423c873c3311ed6618e6d6da33d
TuNguyen111 <23210308@ms.uit.edu.vn>
2024-12-07 09:14:20 +0700

    db: Add constraints and triggers for auto-increment ID (#6)
    - Created constraints in `20_add_constraints.sql`
    - Added function for auto-increment ID in `30_function_for_auto_increase_id.sql`
    - Updated table creation script in `10_create_tables.sql`
    - Added trigger for auto-increment ID in `40_trigger_auto_increase_id.sql`
    
    Co-authored-by: vuongvlq <vuong.vlq.1998@gmail.com>

---
+++ constraints/20_add_constraints.sql
@@ -0,0 +1,44 @@
+-- Kiểm tra số lượng tồn kho >= 0
+ALTER TABLE HangHoa
+ADD CONSTRAINT ck_soluongtonkho CHECK (SoLuongTonKho >= 0);
+
+-- Kiểm tra NgayLap không lớn hơn ngày hiện tại và TongTien > 0
+ALTER TABLE HoaDon
+ADD CONSTRAINT ck_ngaylaphd CHECK (NgayLapHD <= CURRENT_DATE),
+ADD CONSTRAINT ck_tongtien CHECK (TongTien > 0::money);
+
+-- Function kiểm tra phòng ban của nhân viên là 'PB2'
+CREATE OR REPLACE FUNCTION check_phongban_pb2()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Kiểm tra phòng ban của NhanVienID
+    IF (SELECT PhongBan FROM NhanVien WHERE NhanVienID = NEW.NhanVienID) != 'PB2' THEN
+        RAISE EXCEPTION 'NhanVienID % không thuộc phòng ban PB2', NEW.NhanVienID;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_check_phongban_pb2
+BEFORE INSERT OR UPDATE ON HoaDon
+FOR EACH ROW
+EXECUTE FUNCTION check_phongban_pb2();
+
+CREATE OR REPLACE FUNCTION check_ngay_tao_hoa_don() RETURNS TRIGGER AS $$
+DECLARE
+  ngay_vao_lam DATE;
+BEGIN
+  SELECT NgayVaoLam INTO ngay_vao_lam FROM NhanVien WHERE NhanVienID = NEW.NhanVienID;
+
+  IF NEW.NgayLapHD < ngay_vao_lam THEN
+    RAISE EXCEPTION 'Ngày tạo hóa đơn phải lớn hơn ngày vào làm của nhân viên tạo hóa đơn!';
+  END IF;
+
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER ngay_tao_hoa_don_trigger
+  BEFORE INSERT OR UPDATE ON HoaDon
+  FOR EACH ROW
+  EXECUTE FUNCTION check_ngay_tao_hoa_don();
